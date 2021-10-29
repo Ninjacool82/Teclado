ec6e3b0fd5fc1309dafeb353ce162252a8691165
Ninjacool82 <joeproyecto1982@gmail.com>
2021-10-29 17:36:04 -0300

    Entrega

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-function enviarformulario() {
-    console.log("Enviando formulario ...");
+var nombre = document.getElementById("nombre");
+var correo = document.getElementById("correo");
+var error = document.getElementById("error");
+var textarea = document.getElementById("textarea");
+
+error.style.color = "red";
+
+function enviarFormulario() {
+    console.log("Enviando formulario ..");
+
+    var mensajesError = [];
+    if (nombre.value === null || nombre.value === "") {
+        mensajesError.push("Ingresa tu nombre");
+    }
+    if (correo.value === null || correo.value === "") {
+        mensajesError.push("Ingresa tu correo");
+    }
+    if (textarea.value === null || textarea.value === "") {
+        mensajesError.push("Ingresa tu mensaje");
+    }
+    error.innerHTML = mensajesError.join(", ");
     return false;
 }
